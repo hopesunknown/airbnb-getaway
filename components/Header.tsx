1ec717cdc2981e98e1570ad065adf534fb23df63
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-05 00:19:58 -0400

    troubleshoot why Tailwind was not  showing up, needed to add Tailwind directiv es to each of Tailwind's layers to globals.c ss file. Also adjusted some Header section s tyling'troubleshoot why Tailwind was not  showing up, needed to add Tailwind directiv es to each of Tailwind's layers to globals.c ss file. Also adjusted some Header section s tyling'

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image';
+import {MagnifyingGlassCircleIcon} from '@heroicons/react/24/solid';
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <header className='sticky top-0 z-50 grid grid-cols-3 bg-indigo-500 shadow-md p-5'>
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
 
       {/* left section */}
       <div className='relative flex items-center h-10 cursor-pointer my-auto'>
         <Image 
           src='/./../public/logo.png' 
           alt='company logo and name' 
-          style={{objectFit:'contain', objectPosition:'left'}}
+          // style={{objectFit:'contain', objectPosition:'left'}}
           height={50}
           width={50}
           priority
@@ -19,9 +20,9 @@ const Header = (props: Props) => {
       </div>
 
       {/* middle section */}
+      <div className='flex items-center md:border-2 rounded-full py-2'>
         <input type='text' placeholder='Search' />
-      <div>
-
+        <MagnifyingGlassCircleIcon className='hidden md:inline-flex h-8 bg-sky-600 text-white rounded-full p-2 cursor-pointer' />
       </div>
 
       {/* right section */}
