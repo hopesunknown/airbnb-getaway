41440dc86b03fc9a493806dc7b43f8ab398af6f1
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-04 23:59:38 -0400

    beginning to add search bar in Header section and adding grid in this section to properly space out elements visually

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,20 +4,22 @@ type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <header className='sticky top-0 z-50'>
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-indigo-500 shadow-md p-5'>
 
       {/* left section */}
-      <div className='relative flex items-center h-10 cursor-pointer'>
+      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
         <Image 
           src='/./../public/logo.png' 
           alt='company logo and name' 
           style={{objectFit:'contain', objectPosition:'left'}}
-          height='50'
-          width='50'
+          height={50}
+          width={50}
+          priority
         />
       </div>
 
       {/* middle section */}
+        <input type='text' placeholder='Search' />
       <div>
 
       </div>
