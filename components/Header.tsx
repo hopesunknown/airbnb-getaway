f7e3ab7552ca6f4f5df8ab88181ca6306cb1ec4f
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-07 23:52:54 -0400

    imported more Hero Icons to be displayed at right section of Header component. These icons will later be used as menu and account buttons, as well as Become A Host button. Added flex and padding to these buttons.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import {MagnifyingGlassCircleIcon} from '@heroicons/react/24/solid';
+import {MagnifyingGlassCircleIcon, GlobeAltIcon, Bars3Icon, UserCircleIcon, UsersIcon} from '@heroicons/react/24/solid';
 
 type Props = {}
 
@@ -21,13 +21,22 @@ const Header = (props: Props) => {
 
       {/* middle section */}
       <div className='flex-grow items-center md:border-2 rounded-full py-2 md:shadow-xl'>
-        <input className='pl-5 bg-transparent outline-none' type='text' placeholder='Search' />
+        <input 
+          className='pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' 
+          type='text' 
+          placeholder='Search' 
+        />
         <MagnifyingGlassCircleIcon className='hidden md:inline-flex h-8 bg-sky-600 text-white rounded-full p-2 cursor-pointer md:mx-2' />
       </div>
 
       {/* right section */}
-      <div>
-
+      <div className='flex items-center space-x-4 justify-end text-gray-500'>
+        <p>Become A Host</p>
+        <GlobeAltIcon className='h-6 cursor-pointer' />
+        <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+          <Bars3Icon className='h-6'/>
+          <UserCircleIcon className='h-6'/>
+        </div>
       </div>
     </header>
   )
