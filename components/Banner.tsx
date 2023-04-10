4ef5d8fe99bbf3b8b3887a286d3d4b38dbe6c1e6
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-09 23:59:36 -0400

    created responsive banner sizing across various screen sizes

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const Banner = (props: Props) => {
   return (
-    <div>
+    <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
       <Image 
         src='' 
         alt='banner image' 
