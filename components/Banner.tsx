10644ac232d4ad6d664db478e86208ba2438a278
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-10 23:50:30 -0400

    Added image to banner hero component to display on main page. Also added Tailwind styling to make image and center text responsive across different screen sizes. Added text and button to overlay on top of the image.

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,11 +7,16 @@ const Banner = (props: Props) => {
   return (
     <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
       <Image 
-        src='' 
+        src='/./../public/vacationhomebanner.jpeg' 
         alt='banner image' 
         fill 
         style={{objectFit: 'cover'}} 
       />
+      <div className='absolute top-1/2 w-full text-center'>
+        <p className='text-sm sm:text-lg'>GetAway there fast and then take it slow.</p>
+        <p className='text-sm sm:text-lg'>Find your next adventure.</p>
+        <button>Explore</button>
+      </div>
     </div>
   )
 }
