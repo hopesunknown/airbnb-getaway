7d678f512fa49797ab9b2b7abc37518cda499fe8
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-20 23:14:36 -0400

    Resolved error with image fill attribute in SmallCard component. The `fill` attribute needed to be placed within the className as this is Tailwind CSS convention. Placing this outside of className but within the JSX tag was not allowing the image to span the width of the container. In addition, the main page now has SmallCard components rendered into columns for cleaner layout display. Before it was all the city cards in one column. Now, the cards are grouped into more than one column, and with the addition of responsive Tailwind CSS code the number of columns increase as the screen size increases. This allows for city cards to be displayed in a cleaner visual layout.

--- components/SmallCard.tsx
+++ components/SmallCard.tsx
@@ -6,7 +6,7 @@ const SmallCard = (props: {img: string; location: string; distance: string}) => 
   return (
     <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
       <div className='relative h-16 w-16'>
-        <img src={img} alt='city icon' className='rounded-lg' fill />
+        <img src={img} alt='city icon' className='rounded-lg fill' />
       </div>
       <div>
         <h2>{location}</h2>
--- pages/index.tsx
+++ pages/index.tsx
@@ -38,17 +38,16 @@ export default function Home({exploreData}: InferGetStaticPropsType<typeof getSt
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
-          <div>
-
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData?.map((item) => (
+              <SmallCard 
+                key={item.img}
+                img={item.img} 
+                distance={item.distance} 
+                location={item.location}
+              />
+            ))}
           </div>
-          {exploreData?.map((item) => (
-            <SmallCard 
-              key={item.img}
-              img={item.img} 
-              distance={item.distance} 
-              location={item.location}
-            />
-          ))}
         </section>
       </main>
     </>
