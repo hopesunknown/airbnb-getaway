34f02bbaf211dd6bb441ed07c733a6d78ed386f4
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-25 23:33:46 -0400

    Added more details to the LargeCard component. The additional details include passing down the props of img, title, description, and buttonText to display on top of the component, along with the relative/absolute position and Tailwind styling of the elements.

--- components/LargeCard.tsx
+++ components/LargeCard.tsx
@@ -2,10 +2,17 @@
 type Props = {}
 
 const LargeCard = (props: {img: string, title: string, description: string, buttonText: string}) => {
-  return (
-    <section>
-      <div>
+  const {img, title, description, buttonText} = props;
 
+  return (
+    <section className='relative py-16 cursor-pointer'>
+      <div className='relative h-96 min-w-[300px]'>
+        <img src={img} className='rounded-2xl object-cover fill' />
+      </div>
+      <div className='absolute top-32 left-12'>
+        <h3 className='text-4xl mb-3 w-64'>{title}</h3>
+        <p>{description}</p>
+        <button className='text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5'>{buttonText}</button>
       </div>
     </section>
   )
