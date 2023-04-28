a7603aa477e231c77b804d8f41ee9ead9b4c2f2a
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-27 22:40:33 -0400

    Set up the Footer component with four columns of links for About, Community, Host, and Support sections. These footer links will eventually navigate user to those pages of the site. Added grid and spacing/padding to group the sections in the Footer and give cleaner UI layout. Made grid dynamic with number of columns increasing from 1 column to 4 columns as the screen size increases. This change was made for cleaner layout purposes.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,52 +3,39 @@ type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <>
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
       <div className='space-y-4 text-xs text-gray-800'>
         <h5 className='font-bold'>ABOUT</h5>
-        <p>How GetAway Works</p>
-        <p>News</p>
+        <p>How GetAway works</p>
+        <p>Newsroom</p>
         <p>Investors</p>
         <p>GetAway Plus</p>
-        <p>GetAway Platinum</p>
+        <p>Careers</p>
       </div>
 
       <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>ABOUT</h5>
-        <p>How GetAway Works</p>
-        <p>News</p>
-        <p>Investors</p>
-        <p>GetAway Plus</p>
-        <p>GetAway Platinum</p>
+        <h5 className='font-bold'>COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>GetAway: Disaster relief housing</p>
+        <p>Diversity and combating discrimination</p>
       </div>
 
       <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>ABOUT</h5>
-        <p>How GetAway Works</p>
-        <p>News</p>
-        <p>Investors</p>
-        <p>GetAway Plus</p>
-        <p>GetAway Platinum</p>
+        <h5 className='font-bold'>HOST</h5>
+        <p>GetAway your home</p>
+        <p>Explore hosting resources</p>
+        <p>Visit our community forum</p>
+        <p>How to host responsibly</p>
       </div>
 
       <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>ABOUT</h5>
-        <p>How GetAway Works</p>
-        <p>News</p>
-        <p>Investors</p>
-        <p>GetAway Plus</p>
-        <p>GetAway Platinum</p>
+        <h5 className='font-bold'>SUPPORT</h5>
+        <p>Help center</p>
+        <p>Cancellation options</p>
+        <p>Our COVID-19 response</p>
+        <p>Report a neighborhood concern</p>
       </div>
-      
-      <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>ABOUT</h5>
-        <p>How GetAway Works</p>
-        <p>News</p>
-        <p>Investors</p>
-        <p>GetAway Plus</p>
-        <p>GetAway Platinum</p>
-      </div>
-    </>
+    </div>
   )
 }
 
