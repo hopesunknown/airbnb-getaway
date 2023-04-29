f6807f65002a249d5f7eff1652ed2e509f549a75
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-28 22:26:53 -0400

    Reverted Image back to img tags in LargeCard and MediumCard components due to empty string error. After using img tags again the main page displays the image components properly. Will need to figure out how to use NextJS Image again in the future due to image optimization and better performance. Also fixed the search bar layout which had the magnifying glass icon in the center of the input area and moved to the right whenever text was entered into the input area. Now, the icon has been moved to the right of the input area as originally intended, and does not move while text is being typed into the search bar.

--- components/LargeCard.tsx
+++ components/LargeCard.tsx
@@ -8,7 +8,7 @@ const LargeCard = (props: {img: string, title: string, description: string, butt
   return (
     <section className='relative py-16 cursor-pointer'>
       <div className='relative h-96 min-w-[300px]'>
-        <Image src={img} alt='large card with vacation type info' className='rounded-2xl object-cover' fill />
+        <img src={img} alt='large card with vacation type info' className='rounded-2xl object-cover fill' />
       </div>
       <div className='absolute top-32 left-12'>
         <h3 className='text-4xl mb-3 w-64'>{title}</h3>
--- components/MediumCard.tsx
+++ components/MediumCard.tsx
@@ -7,7 +7,7 @@ const MediumCard = (props: {img: string, title: string}) => {
   return (
     <div className='cursor-pointer hover:scale-105 transform transition duration-300 ease-out'>
       <div className='relative h-80 w-80'>
-        <Image src={img} alt='medium card with city info' className='rounded-xl' fill />
+        <img src={img} alt='medium card with city info' className='rounded-xl fill' />
       </div>
       <h3 className='text-2xl mt-3'>{title}</h3>
     </div>
