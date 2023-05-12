d9b89156e82cbc41e57e072e31320d959911b063
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-05-11 22:57:18 -0400

    Setup filter buttons at the top of the Search results page with Tailwind styling. Made reusable utility style class in the global CSS file for filter buttons. This allows for easier changing across several buttons and for uniformity. Added title which will be changed later to be more dynamic after user makes search

--- pages/search.tsx
+++ pages/search.tsx
@@ -9,7 +9,17 @@ const Search = (props: Props) => {
       <Header />
       <main className='flex'>
         <section>
-          <p>300+ Stays for 5 number of guests</p>
+          <p className='text-xs'>300+ Stays for 5 number of guests</p>
+
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+
+          <div className='hidden lg:inline-flex'>
+            <p className='button'>Cancellation flexibility</p>
+            <p className='button'>Place type</p>
+            <p className='button'>Price</p>
+            <p className='button'>Rooms and beds</p>
+            <p className='button'>More filters</p>
+          </div>
         </section>
       </main>
       <Footer />
