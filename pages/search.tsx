afade90ba30a2df074ec41d8ca3c2652e1580dbd
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-05-16 22:51:13 -0400

    Added useRouter from NextJS to Search component. The search query that the user makes will be pulled into search component and accessed here on the page. The props for location, start date, end date, and number of guests are passed via router.query when user makes search and this info is dynamically displayed onto the page. This makes the search page more dynamic

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,17 +1,27 @@
+import {useRouter} from 'next/dist/client/router';
 import Header from './../components/Header.jsx';
 import Footer from './../components/Footer.jsx';
+import {format} from 'date-fns';
 
 type Props = {};
 
 const Search = (props: Props) => {
+  const router = useRouter();
+  const {location, startDate, endDate, noOfGuests} = router.query;
+  const formattedStartDate = format(new Date(startDate), 'dd MMMM yy');
+  const formattedEndDate = format(new Date(endDate), 'dd MMMM yy');
+  const range = `${formattedStartDate} - ${formattedEndDate}`
+
+  // console.log(router.query);
+
   return (
     <div>
       <Header />
       <main className='flex'>
         <section className='flex-grow pt-14 px-6'>
-          <p className='text-xs'>300+ Stays for 5 number of guests</p>
+          <p className='text-xs'>300+ Stays - {range} - for {noOfGuests} guests</p>
 
-          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
 
           <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
             <p className='button'>Cancellation flexibility</p>
