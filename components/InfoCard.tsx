4add6b05be99abf3457d3a453e25b8597bade767
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-05-24 23:48:48 -0400

    Fi xed Typescript errors in InfoCard component, the prop typ es were written as values instead of object types.

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 type Props = {}
 
-const InfoCard = (props = {img: string, location: string, title: string, description: string, star: string, price: string, total: string}) => {
+const InfoCard = (props = {img: String, location: String, title: String, description: String, star: String, price: String, total: String}) => {
   const {img, location, title, description, star, price, total} = props;
   return (
     <div>InfoCard</div>
